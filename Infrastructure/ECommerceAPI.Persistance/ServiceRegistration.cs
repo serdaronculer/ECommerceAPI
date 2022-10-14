7696ee3df5810642ba1fb8d4fa7f3beb101a872f
Serdar <serdar.onculer.work@hotmail.com>
2022-10-14 23:56:57 +0300

    EF Core Entities and database connection done

---
+++ Infrastructure/ECommerceAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ECommerceAPI.Persistance.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ECommerceAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+
+        public static void AddPersistenceServices(this IServiceCollection service)
+        {
+            ConfigurationManager configurationManager = new ConfigurationManager();
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ECommerceAPI.API"));
+            configurationManager.AddJsonFile("appsettings.json");
+
+            service.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(configurationManager.GetConnectionString("PostgreSQL")));
+        }
+    }
+}
