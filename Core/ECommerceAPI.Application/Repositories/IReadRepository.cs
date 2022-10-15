fac3d217d912039599b9b73940e67b3f62f8ac11
Serdar <serdar.onculer.work@hotmail.com>
2022-10-15 16:32:48 +0300

    Added AsNoTracking function for listing queries

--- Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
+++ Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -10,10 +10,10 @@ namespace ECommerceAPI.Application.Repositories
 {
     public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T,bool>> method);
-        Task<T> GetSingleAsync(Expression<Func<T,bool>> method);
-        Task<T> GetByIdAsync(string id);
-        Task<T> GetByIdAsync(Guid id);
+        IQueryable<T> GetAll(bool tracking = true);
+        IQueryable<T> GetWhere(Expression<Func<T,bool>> method, bool tracking = true);
+        Task<T> GetSingleAsync(Expression<Func<T,bool>> method, bool tracking = true);
+        Task<T> GetByIdAsync(string id, bool tracking = true);
+        Task<T> GetByIdAsync(Guid id, bool tracking = true);
     }
 }
--- Infrastructure/ECommerceAPI.Persistance/Repositories/ReadRepository.cs
+++ Infrastructure/ECommerceAPI.Persistance/Repositories/ReadRepository.cs
@@ -23,29 +23,45 @@ namespace ECommerceAPI.Persistance.Repositories
 
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll()
+        public IQueryable<T> GetAll(bool tracking = true)
         {
-            return Table;
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+                return query;                
         }
 
-        public async Task<T> GetByIdAsync(string id)
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
         {
-         return await  Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
         }
 
-        public async Task<T> GetByIdAsync(Guid id)
+        public async Task<T> GetByIdAsync(Guid id, bool tracking = true)
         {
-            return await Table.FirstOrDefaultAsync(data => data.Id == id);
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+                return await query.FirstOrDefaultAsync(data => data.Id == id);
         }
 
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
         {
-            return await Table.FirstOrDefaultAsync(method);
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+             return await query.FirstOrDefaultAsync(method);
+
         }
 
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
         {
-            return Table.Where(method);
+            var query = Table.Where(method);
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
         }
     }
 }
