0ee929c3e3ea413a509fe53fc8f4be6b56d728f3
Serdar <serdar.onculer.work@hotmail.com>
2022-10-21 00:52:28 +0300

    Cors and Fluent Validation configured

--- Presentation/ECommerceAPI.API/Program.cs
+++ Presentation/ECommerceAPI.API/Program.cs
@@ -1,12 +1,30 @@
+using ECommerceAPI.Application.Validators.Products;
+using ECommerceAPI.Application.ViewModels.Products;
+using ECommerceAPI.Infrastructure.Filters;
 using ECommerceAPI.Persistance;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddPersistenceServices();
+//Cors Policy Configuration
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+policy.WithOrigins("http://localhost:8080", "https://localhost:8080").AllowAnyHeader().AllowAnyMethod()
+));
+//  Default filter supressed beacuse I  customized
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true)
+    .AddNewtonsoftJson();
+//Fluent Validation Configuration
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateProductValidator>();
 
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
